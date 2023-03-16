8c376b03a8f7eb6eb2a8e7464ab7f59b2f19e0de
Golujha <gkumarjha304@gmail.com>
2023-03-16 13:36:30 +0530

    first commit

---
+++ OnlinePizzaDelivery/partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0"> DATE-> 15 - 01 - 2023 <a href="https://github.com/Golujha/" target="_blank" rel="noopener noreferrer">Gkumarjha304@gmail.com</a></p>
+</div>
